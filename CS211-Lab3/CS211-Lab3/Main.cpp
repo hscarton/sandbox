674ed514c2551d56628c113d1a301c91954b646b
Halie Carton <hc64@humboldt.edu>
2018-02-12 12:23:18 -0800

    Added Lab 3

--- CS211-Lab3/CS211-Lab3/Main.cpp
+++ CS211-Lab3/CS211-Lab3/Main.cpp
@@ -3,32 +3,77 @@
 #include "StringSplitter.h"
 #include <string>
 #include <fstream>
-#include <ctype>
 #include <string>
 
 using namespace std;
 
 double evaluatePostfixExpression(string postfix_string)
 {
-	double result = -1;
+	double result = 0;
 	stack<double> postfix_stack{};
 	vector<string> pieces = StringSplitter::split(postfix_string, " ");
+        double temp = 0;
 
-	//TODO: evalute pieces as a postfix expression
+	//TODO: evaluate pieces as a postfix expression
 
 	for (auto i : pieces)
 	{
-
+                //cast i as an int and check whether it is a digit
+            
 		if (isdigit(stoi(i)))
 		{
-			postfix_stack.push(stoi(i));
+                    //then push to the stack
+                    
+                    postfix_stack.push(stod(i));
 		}
-		else
+		
+                //if the string is not a digit, it must be an operator
+                //determine which operator it is, and do the operation
+                //on the first two elements in the stack
+                
+                else
 		{
-			
+		    if(i == "+")
+                    {
+                        temp = postfix_stack.top();
+                        postfix_stack.pop();
+                        temp += postfix_stack.top();
+                    }
+                    else if(i == "-")
+                    {
+                        temp = postfix_stack.top();
+                        postfix_stack.pop();
+                        temp -= postfix_stack.top();
+                    }
+                    else if(i == "*")
+                    {
+                        temp = postfix_stack.top();
+                        postfix_stack.pop();
+                        temp = temp * postfix_stack.top();
+                    }
+                    else if(i == "/")
+                    {
+                        temp = postfix_stack.top();
+                        postfix_stack.pop();
+                        temp = temp / postfix_stack.top();
+                    }
 		}
+                /*
+                while(postfix_stack.empty() == false)
+                {
+                    //in STL, we use top() to get the top item, pop()
+                    //returns nothing
+                    cout << postfix_stack.top();
+                    
+                    //pop is only used to remove the top-most item from 
+                    //the stack
+                    //postfix_stack.pop();
+                }
+                 
 	}
-
+        
+        postfix_stack.pop();
+ */
 	return result;
 }
 int main(void)
@@ -50,7 +95,7 @@ int main(void)
 			string current_line;
 			getline(input_file, current_line);
 			//static method/functions do not belong to ANY class of that type
-			//declare funcitons static if they do not operate on any instance
+			//declare functions static if they do not operate on any instance
 			double result = evaluatePostfixExpression(current_line);
 			cout << current_line << " = " << result << endl;
 			
